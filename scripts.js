6502e0dec2558f9fbdf9a5e4e1d5f389491e768a
MrJeanB <jean.j.c@outlook.com>
2022-06-05 09:45:05 -0300

    terminada

--- scripts.js
+++ scripts.js
@@ -13,23 +13,90 @@ class Calculator {
         this.clear();
     }
 
+    formatDisplayNumber(number) {
+        const stringNumber = number.toString();
+
+        const integerDigits = parseFloat(stringNumber.split(".")[0]);
+        const decimalDigits = stringNumber.split(".")[1];
+
+        let integerDisplay;
+
+        if (isNaN(integerDigits)) {
+            integerDisplay = "";
+        } else {
+            integerDisplay = integerDigits.toLocaleString("en", {maximumFractionDigits: 0,});
+        }
+
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`;
+        } else {
+            return integerDisplay;
+        }
+    }
+
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    }
+
+    calculate() {
+        let result;
+
+        const _previousOperand = parseFloat(this.previousOperand);
+        const _currentOperand = parseFloat(this.currentOperand);
+    
+        if (isNaN(_previousOperand) || isNaN(_currentOperand)) return;
+
+        switch (this.operation) {
+            case "+":
+                result = _previousOperand + _currentOperand;
+                break;
+            case "-":
+                result = _previousOperand - _currentOperand;
+                break;
+            case "*":
+                result = _previousOperand *_currentOperand;
+                break;
+            case "÷":
+                result = _previousOperand /  _currentOperand;
+                break;
+            default:
+                return;
+        }
+        this.currentOperand = result;
+        this.operation = undefined;
+        this.previousOperand = "";
+    }
+
+    chooseOperation(operation) {
+        if (this.currentOperand === "") return;
+
+        if (this.previousOperand !== "") {
+            this.calculate();
+        }
+        
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = "";
+    }
+
     appendNumber(number) {
+        if (this.currentOperand.includes(".") && number === ".") return;
+        
         this.currentOperand = `${this.currentOperand}${number.toString()}`;
     }
 
     clear() {
         this.currentOperand = "";
         this.previousOperand = "";
-        this.operantion = undefined;
+        this.operation = undefined;
     }
 
     updateDisplay() {
-        this.previousOperandTextElement.ennerText = this.previousOperand;
-        this.currentOperandTextElement.ennerText = this.currentOperand;
+        this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation || ""}`;
+        this.currentOperandTextElement.innerText = this.formatDisplayNumber(this.currentOperand);
     }
 }
-
-const calculator = new Calculator(
+    const calculator = new Calculator(
     previousOperandTextElement,
     currentOperandTextElement
 );
@@ -41,7 +108,25 @@ for (const numberButton of numberButtons) {
     });
 }
 
+for (const operationButton of operationButtons) {
+    operationButton.addEventListener("click", () => {
+        calculator.chooseOperation(operationButton.innerText);
+        calculator.updateDisplay();
+    });
+}
+
 allClearButton.addEventListener("click", () => {
     calculator.clear();
+    calculator.updateDisplay();
+    
+});
+
+equalsButton.addEventListener("click", () => {
+    calculator.calculate();
+    calculator.updateDisplay();
+});
+
+deleteButton.addEventListener("click", () => {
+    calculator.delete();
     calculator.updateDisplay();
 });
